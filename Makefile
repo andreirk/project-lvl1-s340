3fff63949a82b1e80050f40dd9a77ae60a045b20
andreyirk <sokolik_85@list.ru>
2018-09-10 22:42:41 -0400

    first commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+start:
+	npm run babel-node -- src/bin/brain-games.js
+	
+publish:
+	npm publish --access=public
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+function greet(){
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Welcome, ${userName}!`);
+}
+
+export {greet}
